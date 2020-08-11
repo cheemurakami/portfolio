ba8a42b0c193ccff0c1e541bfed7b3afade87a4e
Chisato Murakami <kiwipom1031@gmail.com>
2020-08-11 10:21:37 -0700

    add more sections in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Container, Media } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import cheeLogo from "../images/cheeLogo.jpg";
+import intern from "../images/intern.png";
 import imageCards from "../cards/imageCards";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 class Home extends React.Component {
   constructor(props) {
@@ -15,34 +14,22 @@ class Home extends React.Component {
     this.galleryImages = null;
   }
 
-  prevImg = () => {
-    this.galleryImages.scrollBy({
-      left: -200,
-      behavior: "smooth"
-    })
-  };
-
-  nextImg = () => {
-    this.galleryImages.scrollBy({
-      left: 200,
-      behavior: "smooth"
-    })
-  };
-
   render() {
-    const { images } = this.state;
     return (
       <React.Fragment>
         <Container>
-          <Media className="mt-5">
-            <img
-              width={150}
-              height={150}
-              className="align-self-start mr-3"
-              src={cheeLogo}
-              alt="Generic placeholder"
-            />
-            <Media.Body>
+          <Row className="mt-3 mb-3">
+            <Col xs={12} md={3} className="mt-3">
+              <img
+                width={150}
+                height={150}
+                className="align-self-start mr-3"
+                src={cheeLogo}
+                alt="cheeLogo"
+              />
+            </Col>
+
+            <Col xs={12} md={9} className="mt-3">
               <h4>Media Heading</h4>
               <p className="pullLeft">
                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
@@ -51,10 +38,54 @@ class Home extends React.Component {
                 condimentum nunc ac nisi vulputate fringilla. Donec lacinia
                 congue felis in faucibus.
               </p>
-            </Media.Body>
-          </Media>
+            </Col>
+          </Row>
 
-          
+          <Row className="mt-3 mb-3">
+          <Col xs={12} md={3} className="mt-3 ">
+              <img
+                width={150}
+                height={150}
+                className="align-self-start"
+                src="https://course_report_production.s3.amazonaws.com/rich/rich_files/rich_files/1880/s300/epicodus-ico.png"
+                alt="epiLogo"
+              />
+            </Col>
+            <Col xs={12} md={9} className="mt-3">
+              <h4>Epicodus</h4>
+              <p className="pullLeft">
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin commodo. Cras purus odio,
+                vestibulum in vulputate at, tempus viverra turpis. Fusce
+                condimentum nunc ac nisi vulputate fringilla. Donec lacinia
+                congue felis in faucibus.
+              </p>
+            </Col>
+            
+          </Row>
+          <Row className="mt-3 mb-3">
+            <Col xs={12} md={3} className="mt-3">
+              <img
+                width={150}
+                height={150}
+                className="align-self-start mr-3"
+                src={intern}
+                alt="internLogo"
+                style={{backgroundColor: "white"}}
+              />
+            </Col>
+
+            <Col xs={12} md={9} className="mt-3">
+              <h4>Internship</h4>
+              <p className="pullLeft">
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin commodo. Cras purus odio,
+                vestibulum in vulputate at, tempus viverra turpis. Fusce
+                condimentum nunc ac nisi vulputate fringilla. Donec lacinia
+                congue felis in faucibus.
+              </p>
+            </Col>
+          </Row>
         </Container>
       </React.Fragment>
     );
