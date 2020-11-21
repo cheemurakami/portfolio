a3cc84ff01ebc1330246b4b8af2db422d064f747
Chisato Murakami <kiwipom1031@gmail.com>
2020-11-21 10:06:48 -0800

    edit home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Badge } from "react-bootstrap";
 import cheeOmma from "../images/cheeOmma.JPG";
 import imageCards from "../cards/imageCards";
 import { Link } from "react-router-dom";
@@ -30,16 +30,32 @@ class Home extends React.Component {
             </Col>
 
             <Col xs={12} md={9} className="mt-3 mb-3">
-              <h4>Hi I'm Chee
-              ヽ(*・ω・)ﾉ</h4>
+              <h4>Hi I'm Chee ヽ(*・ω・)ﾉ</h4>
               <p className="pullLeft">
-              Hi there! I am Chee. I am a junior web developer based in Portland, Oregon. I enjoy creating apps with React. Before my coding bootcamp, I worked as a sales representative at an asian foods distributor, managing accounts for restaurants and markets. Before that I studied English literature in Japan. When I am not coding, I am either playing keyboard, doing cross stitch, or crocheting with my cat Kiwi!
+                Hi there! I'm Chee. I'm a junior web developer based in
+                Portland, Oregon. I enjoy creating apps with React and I'm eager to learn more! When I am not coding, I am either playing keyboard, doing cross
+                stitch, or crocheting with my cat Kiwi! 
               </p>
+              <Badge pill variant="secondary">
+                coding
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                cat
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                music
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                crochet
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                crossstitch
+              </Badge>{" "}
             </Col>
           </Row>
 
           <Row className="mt-5 mb-5">
-          <Col xs={12} md={3} className="mt-3 mb-3">
+            <Col xs={12} md={3} className="mt-3 mb-3">
               <img
                 width={175}
                 height={175}
@@ -51,10 +67,34 @@ class Home extends React.Component {
             <Col xs={12} md={9} className="mt-3 mb-3">
               <h4>Epicodus</h4>
               <p className="pullLeft">
-              During my schooling at <a href="https://www.epicodus.com/" >Epicodus</a> I have created dozens of apps using JavaScript, Ruby on Rails, React with Redux, and others. Learned best practices such as TDD, BDD, and along with over 800 hours of programing work. Recently I built a React and Rails flower shop app with authentication, search/ filters, and CRUD functionality. <Link as={Link} to="/projects">See my projects.</Link>
+                During my schooling at{" "}
+                <a href="https://www.epicodus.com/">Epicodus</a> I have created
+                dozens of apps using JavaScript, Ruby on Rails, React with
+                Redux, and others. Learned best practices such as TDD, BDD, and
+                along with over 800 hours of programing work. Check my personal projects{" "}
+                <Link as={Link} to="/projects">
+                  here!
+                </Link>
               </p>
+              <Badge pill variant="secondary">
+                React JS
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Redux
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Firebase
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Firestore
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Ruby on Rails
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                gems
+              </Badge>{" "}
             </Col>
-            
           </Row>
           <Row className="mt-5 mb-5">
             <Col xs={12} md={3} className="mt-3 mb-3">
@@ -64,15 +104,43 @@ class Home extends React.Component {
                 className="align-self-start mr-3"
                 src="https://wjm.s3.amazonaws.com/maclist/uploads/EyeCue+Lab+LLC_1602893511.png"
                 alt="internLogo"
-                style={{backgroundColor: "white"}}
+                style={{ backgroundColor: "white" }}
               />
             </Col>
 
             <Col xs={12} md={9} className="mt-3 mb-3">
               <h4>Internship</h4>
               <p className="pullLeft">
-              I am currently doing an internship at a company called <a href="https://www.eyecuelab.com/" target="_blank" rel="noopener noreferrer">EyeCue Lab</a>, building an habit tracker app using React Native. I have really enjoyed learning and building the frontend and would like to do something similar.
+                I am currently doing an internship at a company called{" "}
+                <a
+                  href="https://www.eyecuelab.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  EyeCue Lab
+                </a>
+                , building an habit tracker app using React Native. I have
+                really enjoyed learning and building the frontend and would like
+                to do something similar.
               </p>
+              <Badge pill variant="secondary">
+                React Native
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Expo
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Redux Persist
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Redux Saga
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                Node.js
+              </Badge>{" "}
+              <Badge pill variant="secondary">
+                React Native Paper
+              </Badge>{" "}
             </Col>
           </Row>
         </Container>
